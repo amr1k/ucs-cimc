40225d699d29572ea19e6e5f1d9da2ea1298c35d
Amrik Singh <amrik.s@gmail.com>
2019-08-15 22:23:36 +0100

    Working Version 1.0

--- poweroff.py
+++ poweroff.py
@@ -1,16 +1,39 @@
-import requests
 import secrets
-from xml.etree import ElementTree as ET
+import requests
+import sys
 
-url = "https://"+secrets.server+"/nuova"
+#   The follow power states are allowed
+#       "On",
+#       "ForceOff",
+#       "GracefulShutdown",
+#       "ForceRestart",
+#       "Nmi"
+#
+def power(server, state, auth):
+    url = "https://"+server+"/redfish/v1/Systems/FCH1706V07G/Actions/ComputerSystem.Reset"
 
-payload = "<aaaLogin\n    inName=\'"+secrets.username+"\'\n    inPassword=\'"+secrets.password+"\'>\n</aaaLogin>"
-headers = {
-    'Content-Type': "text/plain",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache"
-    }
+    payload = "{\"ResetType\": \""+state+"\"}"
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Basic "+auth,
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        }
 
-response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+    response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+    print response.status_code
+    if response.status_code == 200:
+        return "sucess"
+    else:
+        return "fail"
 
-print(response.text.outCookie)
+if len(sys.argv) == 1:
+    print   "\n\n\nplease use the following as the furst argument: \n\n\
+                - On\n\
+                - ForceOff\n\
+                - GracefulShutdown\n\
+                - ForceRestart\n\
+                - Nmi\n\n\n"
+    exit()
+
+print power(secrets.server, sys.argv[1], secrets.auth)
